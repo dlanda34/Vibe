f7832a88d851673a42cda68387988c7f5287087e
Abraham Bazbaz <bazbaz@usc.edu>
2018-05-20 16:42:11 -0500

    Add files via upload

---
+++ test/test/LogInVC.swift
@@ -0,0 +1,27 @@
+//
+//  LogInVC.swift
+//  test
+//
+//  Created by Abey Bazbaz on 5/18/18.
+//  Copyright © 2018 Leet Tek. All rights reserved.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ test/test/SignUpVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Abey Bazbaz on 5/18/18.
+//  Copyright © 2018 Leet Tek. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+ 
+    @IBOutlet weak var emailTF: UITextField!
+    
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    @IBAction func onRegisterTapped(_ sender: Any) {
+    }
+    
+    
+    
+}
+
